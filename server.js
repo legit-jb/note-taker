635ffcae09c81f7a3ddcd19a98b5aa65a87e8dbc
J Brennan <J_Brennan@me.com>
2021-04-05 15:52:51 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// handle parsing for POST and PUT requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// serve static files
+app.use(express.static("public"));
+
+
+// Listener
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
