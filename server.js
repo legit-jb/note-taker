7e2400fa3c8b435a515ca0f157a15fd9abdf9b41
J Brennan <J_Brennan@me.com>
2021-04-05 16:00:09 -0400

    adding heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 // handle parsing for POST and PUT requests
 app.use(express.json());
